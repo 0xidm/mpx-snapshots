4df2f08b2f42a17048ab1c7a0e4afdac93bbf79e
0xidm <0xidm@protonmail.com>
2023-07-14 19:42:26 -0400

    move into nested filestore, instead of copy

--- src/scripts/fantom-import.py
+++ src/scripts/fantom-import.py
@@ -8,16 +8,17 @@ from chaino.utils import init_logger
 def main():
     init_logger(level="INFO")
 
-    filestore = NestedFilestore("/Volumes/tmp/fantom", [4, 3, 2])
+    filestore_path = os.path.expanduser("~/Data/fantom")
+    filestore = NestedFilestore(filestore_path, [4, 3, 2])
 
-    block_start = 56889636
+    block_start = 54647659
     block_end = 65653613
 
     for index in range(block_start, block_end):
         src_filename = f"/tmp/fantom/fantom-block-{index}.pkl"
         dst_filename = None
         if os.path.exists(src_filename):
-            dst_filename = filestore.put(src_filename, index)
+            dst_filename = filestore.put(src_filename, index, move=True)
             if not dst_filename:
                 logging.getLogger("chaino").error(f"Failed to put {src_filename} as {dst_filename}")
         else:
